7a71dc5f8bd0e5a4d3f600105cac999b097c89c4
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-08-04 23:02:08 +0900

    fix: fix port

--- quickflow_backend/src/services/api.ts
+++ quickflow_backend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8090/api",
+  baseURL: "http://127.0.0.1:3000/api",
 });
 
 export const fetchRailways = async () => {
