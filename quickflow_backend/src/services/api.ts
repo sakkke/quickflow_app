e320634f5aeb91576c51249719784fbfcc493e38
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-08-04 22:52:23 +0900

    feat: add API service

---
+++ quickflow_backend/src/services/api.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8090/api",
+});
+
+export const fetchRailways = async () => {
+  const response = await api.get("/railways");
+  return response.data;
+};
+
+export const fetchStations = async () => {
+  const response = await api.get("/stations");
+  return response.data;
+};
+
+export const fetchRoutes = async () => {
+  const response = await api.get("/routes");
+  return response.data;
+};
+
+export const fetchCrowd = async () => {
+  const response = await api.get("/crowd");
+  return response.data;
+};
+
+export const updateCrowdLevel = async (id: string, crowdLevel: number) => {
+  await api.patch(`/crowd/${id}`, { crowdLevel });
+};
